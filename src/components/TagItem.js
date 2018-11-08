0b1f3efa926e5e032c16254691f4ad335127fa93
Alessandro Di Tecco <alessandro@ditecco.me>
2018-11-09 00:42:46 +0100

    Converted indentation to 2 spaces

--- src/components/TagItem.js
+++ src/components/TagItem.js
@@ -8,34 +8,34 @@ import React from 'react';
 
 
 function TagItem(props) {
-	const {
-		name,
-		count,
-		onClick
-	} = props;
+  const {
+    name,
+    count,
+    onClick
+  } = props;
 
-	return (
-		<a
-			href="#"
-			className="tagItem"
-			onClick={onClick}
-		>
-			<div className="tagItemContainer">
-				<span className="tagItemName">
-					{name}
-				</span>
+  return (
+    <a
+      href="#"
+      className="tagItem"
+      onClick={onClick}
+    >
+      <div className="tagItemContainer">
+        <span className="tagItemName">
+          {name}
+        </span>
 
-				<span className="tagItemCount">
-					{
-						count !== null || undefined ?
-						` (${count})`
-						:
-						null
-					}
-				</span>
-			</div>
-		</a>
-	);
+        <span className="tagItemCount">
+          {
+            count !== null || undefined ?
+            ` (${count})`
+            :
+            null
+          }
+        </span>
+      </div>
+    </a>
+  );
 }
 
 export default TagItem;
