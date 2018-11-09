bbb41fad6ef4e27b7f42b3599ec6d4a11baf7311
Alessandro Di Tecco <alessandro@ditecco.me>
2018-11-09 01:52:23 +0100

    Restructured comp to add edit & delete controls

--- src/components/BookmarkItem.js
+++ src/components/BookmarkItem.js
@@ -12,18 +12,25 @@ function BookmarkItem(props) {
     id,
     url,
     tag,
-    timeStamp
+    timeStamp,
+    onEditClick,
+    onDeleteClick
   } = props;
 
+  const handleDelete = (e) => {
+    // console.log(id);
+    onDeleteClick(id);
+  }
+
   return (
-    <a
-      className="postWrapper"
-      href={url}
-      target='_blank'
+    <article
+      className='postItem'
+      id={id}
     >
-      <article
-        className='postItem'
-        id={id}
+      <a
+        className="linkWrapper"
+        href={url}
+        target='_blank'
       >
         <header className="bookmark-header">
           <h4>{url}</h4>
@@ -32,12 +39,35 @@ function BookmarkItem(props) {
         <div className="bookmark-body">
           {tag}
         </div>
+      </a>
 
-        <footer className="bookmark-footer">
-          {timeStamp}
-        </footer>
-      </article>
-    </a>
+      <div className="bookmark-controls">
+        <ul className="bookmark-controls container">
+          <li className="bookmark-controls-item">
+            <a
+              href="#"
+              className="bookmark-controls__edit"
+              onClick={onEditClick}
+            >
+              edit
+            </a>
+          </li>
+          <li className="bookmark-controls-item">
+            <a
+              href="#"
+              className="bookmark-controls__delete"
+              onClick={handleDelete}
+            >
+              delete
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <footer className="bookmark-footer">
+        {timeStamp}
+      </footer>
+    </article>
   );
 }
 
