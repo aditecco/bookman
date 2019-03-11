460d26a120318c6efd7e0548352de117247fc19e
Alessandro Di Tecco <alessandro@ditecco.me>
2019-03-12 00:57:34 +0100

    Created bookmarks reducer

--- src/reducers/bookmarks.js
+++ src/reducers/bookmarks.js
@@ -1,1 +1,38 @@
+
+/* ---------------------------------
 bookmarks reducer
+--------------------------------- */
+
+import actionIDs from '../actions/';
+
+export function bookmarks(state = [], action) {
+  switch (action.type) {
+    case actionIDs.bookmarks.create:
+
+    const
+      { content, id, tags, timestamp } = action,
+      date = new Date(),
+      splitTags = tags.split(',');
+
+    return [
+        ...state,
+        {
+          href: content,
+          id: Date.now(),
+          tags: splitTags,
+          timestamp: date.toLocaleString(),
+        }
+      ]
+      break;
+
+    case actionIDs.bookmarks.edit:
+      break;
+
+    case actionIDs.bookmarks.delete:
+      break;
+
+    default:
+      return state;
+  }
+
+}
