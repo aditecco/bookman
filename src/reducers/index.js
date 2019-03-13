d3a41db047d7ba04ef66d70330c9e8c19888018f
Alessandro Di Tecco <alessandro@ditecco.me>
2019-03-13 01:09:32 +0100

    Created root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,16 @@
 
 /* ---------------------------------
-Root reducer
+root reducer
 --------------------------------- */
+
+import { combineReducers } from 'redux';
+import bookmarks from './bookmarks';
+import tags from './tags';
+
+
+const rootReducer = combineReducers({
+  bookmarks,
+  tags,
+});
+
+export default rootReducer;
