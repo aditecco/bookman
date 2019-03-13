212264b4ee3c818056d31004132b758da4982664
Alessandro Di Tecco <alessandro@ditecco.me>
2019-03-13 01:08:11 +0100

    Completed store

--- src/store.js
+++ src/store.js
@@ -10,6 +10,10 @@ import bookmarks from './data/mockBookmarks';
 
 const defaultState = { bookmarks };
 
-const store = createStore(rootReducer, defaultState);
+const store = createStore(
+  rootReducer,
+  defaultState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
