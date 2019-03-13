00ec1a5f04e0a5fcc210afc37328632da94d4e2b
Alessandro Di Tecco <alessandro@ditecco.me>
2019-03-14 00:49:12 +0100

    Disabled mock bookmarks

--- src/store.js
+++ src/store.js
@@ -5,10 +5,10 @@ Redux store
 
 import { createStore, compose } from 'redux';
 import rootReducer from './reducers/index';
-import bookmarks from './data/mockBookmarks';
+// import mockBookmarks from './data/mockBookmarks';
 
 
-const defaultState = { bookmarks };
+const defaultState = { bookmarks: [] };
 
 const store = createStore(
   rootReducer,
