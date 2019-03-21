2444550e656bf14d4278f9a44bb13f33eb2a07c4
Alessandro Di Tecco <alessandro@ditecco.me>
2019-03-21 23:23:04 +0100

    Separated bookmark input from tag input,  implemented UUID

--- src/components/BookmarkForm.js
+++ src/components/BookmarkForm.js
@@ -3,8 +3,9 @@
   BookmarkForm
 *******************/
 
-// js
+// deps
 import React, { Component } from 'react';
+import uuidv1 from 'uuid';
 
 // components
 import InputField from './InputField';
@@ -37,12 +38,16 @@ class BookmarkForm extends React.Component {
   }
 
   handleSubmit = (e) => {
+    const id = uuidv1();
+
     // only urlInput is required
     if (this.state.urlInput !== '') {
 
       const { urlInput, tagInput } = this.state;
 
-      this.props.addBookmark(urlInput, tagInput);
+      // this.props.addBookmark(urlInput, tagInput);
+      this.props.addBookmark(urlInput, id);
+      this.props.addTags(tagInput, id);
 
       this.setState({
         urlInput: '',
