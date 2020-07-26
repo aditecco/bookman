4672124181c1506394476bd750696e63c0fea9b9
Alessandro Di Tecco <alessandro@ditecco.me>
2020-07-27 01:35:09 +0200

    Added Firebase deps & init

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,33 @@ index
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-
 import store from "./store";
-
-// comps
+import * as firebase from "firebase/app";
+import "firebase/database";
+import "firebase/auth";
+import "./styles/index.scss";
 import App from "./App";
+
+// firebase initializers
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// firebase db instance
+export const db = firebase.database();
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
+
   document.getElementById("root")
 );
