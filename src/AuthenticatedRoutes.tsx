67153572ff9b8863dfe330a70632377e63bd3ad2
Alessandro Di Tecco <alessandro@ditecco.me>
2020-08-01 01:08:53 +0200

    Added NotificationMessage, Modal, Layout comps

--- src/AuthenticatedRoutes.tsx
+++ src/AuthenticatedRoutes.tsx
@@ -5,10 +5,15 @@ AuthenticatedRoutes
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Home from "./pages/Home";
+import Modal from "./components/Modal/Modal";
+import NotificationMessage from "./components/NotificationMessage/NotificationMessage";
 
 export default function AuthenticatedRoutes() {
   return (
     <Router>
+      <NotificationMessage />
+      <Modal />
+
       <Switch>
         <Route path={"/"} exact component={Home} />
       </Switch>
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,45 @@
+/* ---------------------------------
+Layout
+--------------------------------- */
+
+import React, { ReactFragment, ReactElement } from "react";
+// import Navbar from "../Navbar/Navbar";
+// import AppHeader from "../AppHeader/AppHeader";
+// import AppFooter from "../AppFooter/AppFooter";
+
+interface OwnProps {
+  children: ReactElement | any; // TODO
+  rootClass: string;
+  selected?: number | undefined;
+  hasNav?: boolean;
+  hasHeader?: boolean;
+  hasFooter?: boolean;
+}
+
+export default function Layout({
+  children,
+  rootClass,
+  selected,
+  hasNav = true,
+  hasHeader = true,
+  hasFooter = false,
+}: OwnProps): ReactFragment {
+  return (
+    <>
+      {/* {hasHeader && <AppHeader />} */}
+
+      <div className={"Layout" + " " + rootClass}>
+        <main className={rootClass + "Content"}>{children}</main>
+
+        {/*
+          comes _before_ the nav so that we
+          can  create 'conditional styles' with
+          the adjacent selector
+        */}
+        {/* {hasFooter && <AppFooter />} */}
+
+        {/* {hasNav && <Navbar selected={selected} />} */}
+      </div>
+    </>
+  );
+}
