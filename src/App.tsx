9b350555130d1de4301ed870f71e97585c46aa48
Alessandro Di Tecco <alessandro@ditecco.me>
2020-08-01 02:17:21 +0200

    Added auth observer

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,59 @@
 App
 --------------------------------- */
 
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
 import { connect } from "react-redux";
 import Authentication from "./components/Authentication";
 import Spinner from "./components/Spinner/Spinner";
+import * as firebase from "firebase/app";
+import { setAuthState, stopLoading } from "./redux/actions";
+import { useDispatch } from "react-redux";
+import { log } from "./utils";
 
 const AuthenticatedRoutes = React.lazy(() => import("./AuthenticatedRoutes"));
 
 function App({ authentication: { authenticated }, loading }) {
+  const dispatch = useDispatch();
+  /**
+   * Auth observer
+   */
+
+  useEffect(() => {
+    const observer = firebase.auth().onAuthStateChanged(
+      user => {
+        /**
+         * user should be present in case of
+         *  signup, login or persistent session
+         */
+
+        // prettier-ignore
+        if (user)
+        {
+          log("@@@ user is present");
+          
+          dispatch(setAuthState({
+            authenticated: true,
+            user
+          }))
+        }
+        
+        else
+        {
+          log("@@@, no user is present", user);
+        }
+
+        dispatch(stopLoading());
+      },
+      err => {
+        console.error("@onAuthStateChanged", err);
+
+        dispatch(stopLoading());
+      }
+    );
+
+    return () => observer(); // this will unsubscribe from the obs.
+  }, []);
+
   return loading ? (
     <Spinner />
   ) : authenticated ? (
