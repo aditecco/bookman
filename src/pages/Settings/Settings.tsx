c12cf92dfd122c2ca91caa4d579dc0d1815120d6
Alessandro Di Tecco <alessandro@ditecco.me>
2020-08-19 01:28:34 +0200

    Created Settings & Profile pages, which implement Layout

---
+++ src/pages/Settings/Settings.tsx
@@ -0,0 +1,12 @@
+/* ---------------------------------
+Settings
+--------------------------------- */
+
+import React, { ReactElement } from "react";
+import Layout from "../../components/Layout/Layout";
+
+interface IOwnProps {}
+
+export default function Settings(props: IOwnProps): ReactElement {
+  return <Layout root="Settings">Settings</Layout>;
+}
