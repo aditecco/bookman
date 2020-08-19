988202def1efdda7f53e598494c96c7b7628359d
Alessandro Di Tecco <alessandro@ditecco.me>
2020-08-19 18:28:42 +0200

    Wrapped Authentication in Layout

--- src/components/Authentication/Authentication.tsx
+++ src/components/Authentication/Authentication.tsx
@@ -6,20 +6,24 @@ import React from "react";
 import { signInUser, signUpUser } from "../../redux/actions";
 import AuthForm from "../AuthForm/AuthForm";
 import TabSwitcher from "../TabSwitcher/TabSwitcher";
+import Layout from "../Layout/Layout";
+import Navbar from "../Navbar/Navbar";
 
 export default function Authentication() {
   return (
-    <TabSwitcher
-      tabs={[
-        {
-          name: "Login",
-          content: <AuthForm intent="login" actionHandler={signInUser} />,
-        },
-        {
-          name: "Signup",
-          content: <AuthForm intent="signup" actionHandler={signUpUser} />,
-        },
-      ]}
-    />
+    <Layout root="Authentication">
+      <TabSwitcher
+        tabs={[
+          {
+            name: "Login",
+            content: <AuthForm intent="login" actionHandler={signInUser} />,
+          },
+          {
+            name: "Signup",
+            content: <AuthForm intent="signup" actionHandler={signUpUser} />,
+          },
+        ]}
+      />
+    </Layout>
   );
 }
