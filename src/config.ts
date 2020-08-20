64d7f9507a633f180547698a8a01f410f66afcb2
Alessandro Di Tecco <alessandro@ditecco.me>
2020-08-20 16:33:35 +0200

    Changed button type based on option value, minor changes

--- src/config.ts
+++ src/config.ts
@@ -5,12 +5,14 @@ config
 import { TSettings } from "./types/bookman";
 
 export const settings: TSettings = {
+  // TODO maybe create util to convert camelCase => title case
+  // and write as camelCase
   show_descriptions: true,
   lorem_ipsum: false,
   dolor_sit_amet: false,
 };
 
-export function updateSettings(prevSettings, settings): TSettings {
+export function updateSettings(prevSettings, settings) {
   return {
     ...prevSettings,
     ...settings,
--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -27,6 +27,7 @@ export default function Settings(props: IOwnProps): ReactElement {
             {/* {setting}: {String(value)} */}
 
             <BaseButton
+              className={value ? "" : "button--outline"}
               onClick={() => {
                 updateSettings({
                   [setting]: !value,
