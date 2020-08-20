65a71d2ddbc5caf84a0d7ab978327db4f8544cf7
Alessandro Di Tecco <alessandro@ditecco.me>
2020-08-20 03:15:02 +0200

    Removed dep, cleanup

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,7 @@
-/******************
-	Constants
-*******************/
+/* ---------------------------------
+Constants
+--------------------------------- */
 
-import stringify from "json-stringify-safe";
-import mockBookmarks from "./data/mockBookmarks";
-
-export const LOCAL_BOOKMARKS = localStorage.getItem("localBookmarks");
-export const LOCAL_TAGS = localStorage.getItem("localTags");
-export const LOCAL_FOUND = "Found local bookmarks. Rendering...";
-export const LOCAL_NOT_FOUND = "Local bookmarks not found. Initializing...";
-export const INITIAL = stringify([]);
-export const TEST_BOOKMARKS = stringify(mockBookmarks);
 export const MESSAGE__CONFIRM_DELETION =
   "Do you really want to delete this bookmark?";
 export const urlFilter = /https?\:\/\/(?:www\.)?/;
